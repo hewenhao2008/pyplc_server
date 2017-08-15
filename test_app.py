0c527c5fe3c8f286607556d27484e18086642bf2
yakumo17s <wo409697458@gmail.com>
2017-08-15 11:53:42 +0800

    db

--- test_app.py
+++ test_app.py
@@ -30,7 +30,7 @@ def hello():
     # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://%s:%s@%s:%s/%s' \
     #                           % (sae.const.MYSQL_USER, sae.const.MYSQL_PASS,
     #                              sae.const.MYSQL_HOST, int(sae.const.MYSQL_PORT), sae.const.MYSQL_DB)
-    # cursor = db.cursor()
+    cursor = db.cursor()
 
 
     # cursor.execute('select * from user')
