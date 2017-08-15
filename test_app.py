3af913e32a6f04dfbd18e6510c5a9bb33c9914f4
yakumo17s <wo409697458@gmail.com>
2017-08-15 14:21:32 +0800

    db

--- test_app.py
+++ test_app.py
@@ -3,7 +3,7 @@ import json
 import traceback
 from flask import Flask, g, request, jsonify
 # from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 from sae.const import (MYSQL_HOST, MYSQL_HOST_S,
                        MYSQL_PORT, MYSQL_USER, MYSQL_PASS, MYSQL_DB
@@ -15,20 +15,22 @@ print(_mysql.__version__)
 
 app = Flask(__name__)
 app.debug = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://%s:%s@%s:%s/%s' % (
-MYSQL_USER, MYSQL_PASS, MYSQL_HOST, MYSQL_PORT, MYSQL_DB)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://%s:%s@%s:%s/%s' % (
+# MYSQL_USER, MYSQL_PASS, MYSQL_HOST, MYSQL_PORT, MYSQL_DB)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://web:web@localhost:3306/pyplc'
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_RECORD_QUERIES'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 # extensions
 db = SQLAlchemy(app)
 
 
-@app.before_request
-def before_request():
+# @app.before_request
+# def before_request():
     # g.db = SQLAlchemy(app)
-    g.db = MySQLdb.connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASS,
-                          MYSQL_DB, port=int(MYSQL_PORT), charset="utf8")
+    # g.db = MySQLdb.connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASS,
+    #                       MYSQL_DB, port=int(MYSQL_PORT), charset="utf8")
 
 
 @app.teardown_request
@@ -55,13 +57,16 @@ class User(db.Model):
 
 @app.route('/test')
 def test():
-    admin = User('admin', 'admin@example.com')
-    guest = User('guest', 'guest@example.com')
-    db.session.add(admin)
-    db.session.add(guest)
-    db.session.commit()
-    users = User.query.all()
-    admin2 = User.query.filter_by(username='admin').first()
+    # admin = User('admin', 'admin@example.com')
+    # guest = User('guest', 'guest@example.com')
+    # db.session.add(admin)
+    # db.session.add(guest)
+    # db.session.commit()
+    # users = User.query.all()
+    admin2 = User.query.first()
     html = "test result:"
 
     return admin2.username
+
+if __name__ == '__main__':
+    app.run(debug=True)
