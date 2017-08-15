c3c135037f90305d5b1cadffa62cf752b2a6fc8f
yakumo17s <wo409697458@gmail.com>
2017-08-15 11:31:36 +0800

    db

--- blog/app/__init__.py
+++ blog/app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask.ext.mail import Mail
 from flask.ext.sqlalchemy import SQLAlchemy
-from config import config
+from ..config import config
 from flask.ext.login import LoginManager
 
 mail = Mail()
@@ -18,10 +18,8 @@ def create_app(config_name):
     config[config_name].init_app(app)
 
     mail.init_app(app)
-    moment.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
-    pagedown.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
