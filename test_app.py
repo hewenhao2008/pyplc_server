d23e9775cd2761d880e863f15460cf0e5ac0d57b
yakumo17s <wo409697458@gmail.com>
2017-08-15 12:19:52 +0800

    db

--- test_app.py
+++ test_app.py
@@ -37,7 +37,7 @@ def teardown_request(exception):
 
 
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
