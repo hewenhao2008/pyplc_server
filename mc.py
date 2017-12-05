f0c01a03d64cc4c354c72904fa5f45ba8f886ce8
yakumo17s <wo409697458@gmail.com>
2017-12-05 10:01:46 +0800

    memcached

--- mc.py
+++ mc.py
@@ -3,11 +3,12 @@
 import pylibmc as memcache
 
 # 初始化memcache连接
-mc = memcache.Client(
-    ['127.0.0.1'],
-    binary=True,
-    behaviors={
-        'tcp_nodelay': True,
-        'ketama': True
-    }
-)
+# mc = memcache.Client(
+#     ['127.0.0.1'],
+#     binary=True,
+#     behaviors={
+#         'tcp_nodelay': True,
+#         'ketama': True
+#     }
+# )
+mc = memcache.Client()
